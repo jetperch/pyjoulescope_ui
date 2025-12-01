e4595b5ac05fb2b917adeff828046b14af1f4c1d
Matt Liberty <matt.liberty@jetperch.com>
2025-12-01 16:44:19 -0500

    Bumped About copyright range.

--- joulescope_ui/about.py
+++ joulescope_ui/about.py
@@ -1,4 +1,4 @@
-# Copyright 2018-2024 Jetperch LLC
+# Copyright 2018-2025 Jetperch LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ ABOUT = f"""\
 </table>
 
 <pre>
-Copyright 2018-2024 Jetperch LLC
+Copyright 2018-2025 Jetperch LLC
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
